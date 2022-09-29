722436a84fd208eacab58e8ddfe7a94fdc8f0c3f
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-09-29 17:35:13 +0530

    Init

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_talks/slides/slide1.dart';
+import 'package:flutter_talks/theme.dart';
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(ChangeNotifierProvider<ThemeNotifier>(
+      create: (_) => ThemeNotifier(ThemeNotifier.darkTheme),
+      child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalyticsObserver observer =
+  FirebaseAnalyticsObserver(analytics: analytics);
+
+  @override
+  Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      navigatorObservers: <NavigatorObserver>[observer],
+      theme: themeNotifier.getTheme(),
+      home: const Slide1(),
+    );
+  }
+}
+
+
