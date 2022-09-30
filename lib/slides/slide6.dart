0c89977fb5cf198d2f29d9581c635c7fde1cf979
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-01 04:05:43 +0530

    Create slide6.dart

---
+++ lib/slides/slide6.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_talks/base/base1.dart';
+import 'package:flutter_talks/sizes.dart';
+import 'package:flutter_talks/widgets.dart';
+import 'dart:html' as html;
+
+
+class Slide1 extends StatefulWidget {
+  const Slide1({Key? key}) : super(key: key);
+
+  @override
+  State<Slide1> createState() => _Slide1State();
+}
+
+class _Slide1State extends State<Slide1> {
+  @override
+  Widget build(BuildContext context) {
+    return Base1(
+      children: [
+        Row(
+          
+        )
+      ],
+    );
+  }
+}
