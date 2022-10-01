204ad0d369fabec10d2f27b5415b1e6843aa52f9
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-01 23:50:53 +0530

    Update base3.dart

--- lib/base/base3.dart
+++ lib/base/base3.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_talks/sizes.dart';
+import 'package:flutter_talks/slideSelector.dart';
 
 import '../widgets.dart';
 
@@ -14,56 +15,58 @@ class Base3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton.large(
-        onPressed: (){},
-        child: const FlutterLogo(
-          size: 50,
+    return SlideSelector(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton.large(
+          onPressed: (){},
+          child: const FlutterLogo(
+            size: 50,
+          ),
         ),
-      ),
 
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            width: screenWidth(context, mulBy: 0.5),
-            child: ListView(
-              padding: EdgeInsets.symmetric(
-                  horizontal: screenWidth(context, mulBy: 0.05),
-                  vertical: screenHeight(context, mulBy: 0.08)
-              ),
-              children: [
-                Text(
-                head!,
-                style: Theme.of(context).textTheme.displayMedium
-            ),
+        body: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             SizedBox(
-              height: screenHeight(context, mulBy: 0.01),
-            ),
-            Align(alignment:Alignment.topLeft,child: underline()),
-                SizedBox(
-                  height: screenHeight(context, mulBy: 0.1),
+              width: screenWidth(context, mulBy: 0.5),
+              child: ListView(
+                padding: EdgeInsets.symmetric(
+                    horizontal: screenWidth(context, mulBy: 0.05),
+                    vertical: screenHeight(context, mulBy: 0.08)
                 ),
-                ...children
-              ],
-            ),
-          ),
-          Expanded(
-            child: Container(
-              height: screenHeight(context),
-              decoration: const BoxDecoration(
-                color: Color(0xff074177),
-
+                children: [
+                  Text(
+                  head!,
+                  style: Theme.of(context).textTheme.displayMedium
               ),
-              clipBehavior: Clip.antiAlias,
-              child: child??Image.asset(
-                image!,
+              SizedBox(
+                height: screenHeight(context, mulBy: 0.01),
+              ),
+              Align(alignment:Alignment.topLeft,child: underline()),
+                  SizedBox(
+                    height: screenHeight(context, mulBy: 0.1),
+                  ),
+                  ...children
+                ],
               ),
             ),
-          ),
+            Expanded(
+              child: Container(
+                height: screenHeight(context),
+                decoration: const BoxDecoration(
+                  color: Color(0xff074177),
 
-        ],
-      )
+                ),
+                clipBehavior: Clip.antiAlias,
+                child: child??Image.asset(
+                  image!,
+                ),
+              ),
+            ),
+
+          ],
+        )
+      ),
     );
   }
 }
