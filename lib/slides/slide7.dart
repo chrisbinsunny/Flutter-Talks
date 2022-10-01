d3d177efbe5f4fdfc579b74e7a32701696aff128
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-01 15:42:44 +0530

    Slide7 done

--- lib/slides/slide7.dart
+++ lib/slides/slide7.dart
@@ -7,7 +7,6 @@ import 'dart:html' as html;
 
 import '../base/base2.dart';
 
-
 class Slide7 extends StatefulWidget {
   const Slide7({Key? key}) : super(key: key);
 
@@ -20,16 +19,42 @@ class _Slide7State extends State<Slide7> {
   Widget build(BuildContext context) {
     return Base3(
       head: "Flutter is Google’s UI Framework",
-      image: "assets/gif/ui1.gif",
+      image: "assets/gif/hotR.gif",
       children: [
+        Text("Sub-second reload times",
+            style: Theme.of(context).textTheme.headlineSmall),
         SizedBox(
-          width: screenWidth(context, mulBy: 0.4),
-          child: Text(
-              "Helping developers craft high-quality native experiences across multiple platforms in record time.",
-              style: Theme.of(context).textTheme.bodyMedium
-          ),
+          height: screenHeight(context, mulBy: 0.035),
         ),
-
+        Text("Paint your app to life",
+            style: Theme.of(context).textTheme.headlineSmall),
+        SizedBox(
+          height: screenHeight(context, mulBy: 0.035),
+        ),
+        Text("Iterate rapidly on features",
+            style: Theme.of(context).textTheme.headlineSmall),
+        SizedBox(
+          height: screenHeight(context, mulBy: 0.035),
+        ),
+        Text("Test hypotheses quicker than ever",
+            style: Theme.of(context).textTheme.headlineSmall),
+        SizedBox(
+          height: screenHeight(context, mulBy: 0.035),
+        ),
+        Text("More time to experiment & test features",
+            style: Theme.of(context).textTheme.headlineSmall),
+        SizedBox(
+          height: screenHeight(context, mulBy: 0.035),
+        ),
+        Text("Single-codebase for faster collab",
+            style: Theme.of(context).textTheme.headlineSmall),
+        SizedBox(
+          height: screenHeight(context, mulBy: 0.035),
+        ),
+        Text("3X Productivity Gains",
+            style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+              fontWeight: FontWeight.w900
+            )),
       ],
     );
   }
