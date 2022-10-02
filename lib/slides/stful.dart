37c9d4eac7ad459f0be1cf4affe9c929bb5407dc
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-03 02:45:37 +0530

    Update stful.dart

--- lib/slides/stful.dart
+++ lib/slides/stful.dart
@@ -7,7 +7,6 @@ import 'dart:html' as html;
 import '../base/base2.dart';
 import '../base/base5.dart';
 
-
 class Stateful extends StatefulWidget {
   const Stateful({Key? key}) : super(key: key);
 
@@ -18,34 +17,14 @@ class Stateful extends StatefulWidget {
 class _StatefulState extends State<Stateful> {
   @override
   Widget build(BuildContext context) {
-    return Base5(
+    return const Base5(
       head: "Stateful Widget",
       children: [
-        SizedBox(
-          height: screenHeight(context, mulBy: 0.1),
-        ),
-        Text("✔️   Dynamic widget which has its own properties",
-            style: Theme.of(context).textTheme.bodyMedium),
-        SizedBox(
-          height: screenHeight(context, mulBy: 0.035),
-        ),
-        Text("✔️   Those property is known as state of the widget",
-            style: Theme.of(context).textTheme.bodyMedium),
-        SizedBox(
-          height: screenHeight(context, mulBy: 0.035),
-        ),
-        Text("✔️   The state of the widget can be changed by user input, any operation or other widget’s state change",
-            style: Theme.of(context).textTheme.bodyMedium),
-        SizedBox(
-          height: screenHeight(context, mulBy: 0.035),
-        ),
-        Text("✔️   setState() method is used to change the state",
-            style: Theme.of(context).textTheme.bodyMedium),
-        SizedBox(
-          height: screenHeight(context, mulBy: 0.035),
-        ),
-        Text("✔️   Each time widget gets rebuild when setState() method gets called",
-            style: Theme.of(context).textTheme.bodyMedium),
+        "Dynamic widget which has its own properties",
+        "Those property is known as state of the widget",
+        "The state of the widget can be changed by user input, any operation or other widget’s state change",
+        "setState() method is used to change the state",
+        "Each time widget gets rebuild when setState() method gets called",
       ],
     );
   }
