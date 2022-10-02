3c43f52c53c123a606f67722861fb3e7f21ecae1
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-03 01:45:08 +0530

    Update head.dart

--- lib/base/head.dart
+++ lib/base/head.dart
@@ -1,42 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_talks/slideSelector.dart';
 
 import '../sizes.dart';
 
-class Heading extends StatelessWidget {
-  const Heading({Key? key, required this.heading}) : super(key: key);
+class HeadingBase extends StatelessWidget {
+  const HeadingBase({Key? key, required this.heading}) : super(key: key);
 
   final String heading;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-            horizontal: screenWidth(context, mulBy: 0.05),
-            vertical: screenHeight(context, mulBy: 0.08)
-        ),
-        child: SizedBox(
-          height: screenHeight(context),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                child: FlutterLogo(
-                  size: screenHeight(context, mulBy: 0.5),
+    return SlideSelector(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: screenWidth(context, mulBy: 0.05),
+              vertical: screenHeight(context, mulBy: 0.08)
+          ),
+          child: SizedBox(
+            height: screenHeight(context),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: FlutterLogo(
+                    size: screenHeight(context, mulBy: 0.5),
+                  ),
                 ),
-              ),
-              Expanded(
-                  child: Text(
-                    heading,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                      fontSize: 60
-                    ),
-                  )
+                Expanded(
+                    child: FittedBox(
+                      child: Text(
+                        heading,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          fontSize: 60
+                        ),
+                      ),
+                    )
 
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
