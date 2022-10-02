8bfba58147d93f3633b90fedd84af4de04516a1c
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-02 18:28:42 +0530

    Create base4.dart

---
+++ lib/base/base4.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_talks/sizes.dart';
+import 'package:flutter_talks/slideSelector.dart';
+
+
+
+class Base4 extends StatelessWidget {
+  const Base4({Key? key, required this.children}) : super(key: key);
+
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideSelector(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton.large(
+          onPressed: (){},
+          child: const FlutterLogo(
+            size: 50,
+          ),
+        ),
+
+        body: SizedBox(
+          width: double.maxFinite,
+          height: double.maxFinite,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: children,
+          ),
+        )
+      ),
+    );
+  }
+}
+
