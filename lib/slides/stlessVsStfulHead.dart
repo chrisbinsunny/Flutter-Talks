a8eff6be825e2fd5b323f3cddf84678dd1fd6668
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-03 01:03:45 +0530

    Create stlessVsStfulHead.dart

---
+++ lib/slides/stlessVsStfulHead.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_talks/base/head.dart';
+
+class StlessVsStful extends StatelessWidget {
+  const StlessVsStful({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Heading(heading: "\"Stateless Widget\nvs\nStateful Widget\"");
+  }
+}
