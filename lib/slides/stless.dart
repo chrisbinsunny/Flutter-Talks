c06dd99bce7f9c0b07d22967fbde60ff725addb1
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-03 02:45:41 +0530

    Update stless.dart

--- lib/slides/stless.dart
+++ lib/slides/stless.dart
@@ -7,7 +7,6 @@ import 'dart:html' as html;
 import '../base/base2.dart';
 import '../base/base5.dart';
 
-
 class Stateless extends StatefulWidget {
   const Stateless({Key? key}) : super(key: key);
 
@@ -18,34 +17,14 @@ class Stateless extends StatefulWidget {
 class _StatelessState extends State<Stateless> {
   @override
   Widget build(BuildContext context) {
-    return Base5(
+    return const Base5(
       head: "Stateless Widget",
       children: [
-        SizedBox(
-          height: screenHeight(context, mulBy: 0.1),
-        ),
-        Text("✔️   Dumb Widget",
-            style: Theme.of(context).textTheme.bodyMedium),
-        SizedBox(
-          height: screenHeight(context, mulBy: 0.035),
-        ),
-        Text("✔️   It doesn’t know anything",
-            style: Theme.of(context).textTheme.bodyMedium),
-        SizedBox(
-          height: screenHeight(context, mulBy: 0.035),
-        ),
-        Text("✔️   Gets rendered only once or",
-            style: Theme.of(context).textTheme.bodyMedium),
-        SizedBox(
-          height: screenHeight(context, mulBy: 0.035),
-        ),
-        Text("✔️   When the parent widget changes the configuration",
-            style: Theme.of(context).textTheme.bodyMedium),
-        SizedBox(
-          height: screenHeight(context, mulBy: 0.035),
-        ),
-        Text("✔️   Useful for code reusing",
-            style: Theme.of(context).textTheme.bodyMedium),
+        "Dumb Widget",
+        "It doesn’t know anything",
+        "Gets rendered only once or",
+        "When the parent widget changes the configuration",
+        "Useful for code reusing",
       ],
     );
   }
