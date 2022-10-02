4e39c40024bb6d8635d1b498838a1440d3d30ec1
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-03 03:25:51 +0530

    Create widgetsEverywhere.dart

---
+++ lib/slides/widgetsEverywhere.dart
@@ -0,0 +1,36 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_talks/sizes.dart';
+import 'package:flutter_talks/slideSelector.dart';
+
+
+
+class WidgetImage extends StatelessWidget {
+  const WidgetImage({Key? key, }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideSelector(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton.large(
+          onPressed: (){
+            window.open('https://flutter.dev', 'new tab');
+          },
+          child: const FlutterLogo(
+            size: 50,
+          ),
+        ),
+        body: Center(
+          child: Image.asset(
+            "assets/images/widgets.png",
+            fit: BoxFit.fitHeight,
+            height: screenHeight(context),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
