d40677470b460f9228715a9ded61cf31add7425c
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-04 20:54:56 +0530

    Update appCases.dart

--- lib/slides/appCases.dart
+++ lib/slides/appCases.dart
@@ -114,3 +114,98 @@ class AppCases extends StatelessWidget {
   }
 }
 
+
+
+class GpayCase extends StatelessWidget {
+  const GpayCase({Key? key, }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideSelector(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton.large(
+          onPressed: (){
+            window.open('https://flutter.dev', 'new tab');
+          },
+          child: const FlutterLogo(
+            size: 50,
+          ),
+        ),
+        body: Center(
+
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: screenHeight(context, mulBy: 0.8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  boxShadow: const [
+                    BoxShadow(
+                        color: Colors.black38,
+                        blurRadius: 10,
+                        spreadRadius: 5,
+                        offset: Offset(0, 3)
+                    )
+                  ],
+                ),
+                clipBehavior: Clip.antiAlias,
+                child: Image.asset(
+                  "assets/images/gpay.jpg",
+                  fit: BoxFit.fitHeight,
+                  height: screenHeight(context),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    height: screenHeight(context, mulBy: 0.4),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.black38,
+                            blurRadius: 10,
+                            spreadRadius: 5,
+                            offset: Offset(0, 3)
+                        )
+                      ],
+                    ),
+                    clipBehavior: Clip.antiAlias,
+                    child: Image.asset(
+                      "assets/images/gpay1.png",
+                      fit: BoxFit.fitHeight,
+                      height: screenHeight(context),
+                    ),
+                  ),
+                  Container(
+                    height: screenHeight(context, mulBy: 0.4),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.black38,
+                            blurRadius: 10,
+                            spreadRadius: 5,
+                            offset: Offset(0, 3)
+                        )
+                      ],
+                    ),
+                    clipBehavior: Clip.antiAlias,
+                    child: Image.asset(
+                      "assets/images/gpay2.png",
+                      fit: BoxFit.fitHeight,
+                      height: screenHeight(context),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
