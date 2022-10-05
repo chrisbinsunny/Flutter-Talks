d7125a286b6d177b452093079002bc35d477dd31
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-05 18:03:33 +0530

    Update hotReloadHead.dart

--- lib/slides/hotReloadHead.dart
+++ lib/slides/hotReloadHead.dart
@@ -1,5 +1,11 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_talks/base/head.dart';
+
+import '../sizes.dart';
+import '../slideSelector.dart';
 
 class HotReloadHead extends StatelessWidget {
   const HotReloadHead({Key? key}) : super(key: key);
@@ -7,5 +13,33 @@ class HotReloadHead extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const HeadingBase(heading: "\"Hot Reload\"");
+  }
+}
+
+class HotReload extends StatelessWidget {
+  const HotReload({Key? key, }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideSelector(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton.large(
+          onPressed: (){
+            window.open('https://flutter.dev', 'new tab');
+          },
+          child: const FlutterLogo(
+            size: 50,
+          ),
+        ),
+        body: Center(
+          child: Image.asset(
+            "assets/gif/hotR.gif",
+            fit: BoxFit.fitHeight,
+            height: screenHeight(context),
+          ),
+        ),
+      ),
+    );
   }
 }
