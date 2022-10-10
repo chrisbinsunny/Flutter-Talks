9f57196734c14bed50f47f860e1d2071a4f65c9f
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-10 14:49:48 +0530

    Update slide3.dart

--- lib/slides/slide3.dart
+++ lib/slides/slide3.dart
@@ -23,56 +23,59 @@ class _Slide3State extends State<Slide3> {
         SizedBox(
           height: screenHeight(context, mulBy: 0.05),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "      “To the metal” approaches",
-                    style: Theme.of(context).textTheme.displaySmall?.copyWith(
+        SizedBox(
+          width: screenWidth(context),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "      “To the metal” approaches",
+                      style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                          color: Colors.blueGrey,
+                      ),
+                    ),
+                    SizedBox(
+                      height: screenHeight(context, mulBy: 0.04),
+                    ),
+                    const ListContent(head: "High-quality apps", sub: "Platform and system integrations", check: true),
+                    const ListContent(head: "High-performance UIs", sub: "Native code, GPU accelerated", check: true),
+                    const ListContent(head: "Must fund two apps", sub: "Two teams, codebases, & investments", check: false),
+                    const ListContent(head: "Inconsistent brand, features", sub: "Different across devices & OEMs", check: false),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: screenWidth(context, mulBy: 0.1),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "      “Cross platform” approaches",
+                      style: Theme.of(context).textTheme.displaySmall?.copyWith(
                         color: Colors.blueGrey,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: screenHeight(context, mulBy: 0.04),
-                  ),
-                  const ListContent(head: "High-quality apps", sub: "Platform and system integrations", check: true),
-                  const ListContent(head: "High-performance UIs", sub: "Native code, GPU accelerated", check: true),
-                  const ListContent(head: "Must fund two apps", sub: "Two teams, codebases, & investments", check: false),
-                  const ListContent(head: "Inconsistent brand, features", sub: "Different across devices & OEMs", check: false),
-                ],
-              ),
-            ),
-            SizedBox(
-              width: screenWidth(context, mulBy: 0.1),
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "      “Cross platform” approaches",
-                    style: Theme.of(context).textTheme.displaySmall?.copyWith(
-                      color: Colors.blueGrey,
+                    SizedBox(
+                      height: screenHeight(context, mulBy: 0.04),
                     ),
-                  ),
-                  SizedBox(
-                    height: screenHeight(context, mulBy: 0.04),
-                  ),
-                  const ListContent(head: "Fast development", sub: "Same codebase, fast deployment", check: true),
-                  const ListContent(head: "Portability, reach", sub: "Single codebase", check: true),
-                  const ListContent(head: "Poor Performance", sub: "Slow, jerky, unpredictable", check: false),
-                  const ListContent(head: "Non-Native Look/Feel, features", sub: "Users can tell the difference", check: false),
-                ],
+                    const ListContent(head: "Fast development", sub: "Same codebase, fast deployment", check: true),
+                    const ListContent(head: "Portability, reach", sub: "Single codebase", check: true),
+                    const ListContent(head: "Poor Performance", sub: "Slow, jerky, unpredictable", check: false),
+                    const ListContent(head: "Non-Native Look/Feel, features", sub: "Users can tell the difference", check: false),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         )
       ],
     );
