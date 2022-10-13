03fb1c21ea4f37e34160b68723ffe5e57c83e206
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-13 22:10:27 +0530

    Dependency

--- lib/slides/slide4.dart
+++ lib/slides/slide4.dart
@@ -24,81 +24,86 @@ class _Slide4State extends State<Slide4> {
         SizedBox(
           height: screenHeight(context, mulBy: 0.2),
         ),
-        IntrinsicHeight(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                  "“  ",
-                  style: GoogleFonts.spaceGrotesk(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 100,
-                    color: Colors.blue,
-
-                  )
-              ),
-              SizedBox(
-                width: screenWidth(context, mulBy: 0.45),
-                child: RichText(
-                  text: TextSpan(
-                    text: "Flutter combines\n",
-                    style: Theme.of(context).textTheme.bodyLarge,
-                    children: [
-                      TextSpan(
-                        text: "native performance and quality ",
-                          style: GoogleFonts.kumbhSans(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 48,
-                            color: Colors.blue,
-                          )
-                      ),
-                      const TextSpan(
-                          text: "with "
-                      ),
-                      TextSpan(
-                          text: "high-velocity development ",
-                          style: GoogleFonts.kumbhSans(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 48,
-                            color: Colors.blue,
-                          )
-                      ),
-                      const TextSpan(
-                          text: "and "
-                      ),
-                      TextSpan(
-                          text: "multi-platform reach",
-                          style: GoogleFonts.kumbhSans(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 48,
-                            color: Colors.blue,
-                          )
-                      ),
-                      const TextSpan(
-                          text: "."
-                      ),
-                    ]
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Text(
-                    "  ”",
-                    softWrap: true,
-                    textAlign: TextAlign.start,
-
-                    style: GoogleFonts.spaceGrotesk(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 100,
-                      color: Colors.blue,
-
-                    )
-                ),
-              ),
-            ],
-          ),
-        )
+        SizedBox(
+          width: screenWidth(context, mulBy: 0.45),
+          child: Text(""
+              "uhlkfg;ljkdf;djgfs kjdfoihsdof ifjeisofhj  ifjdhifihsfo  hfosdh  hrioeshfoieh fhfoihsd ouifhi sfhesohfsdihfs f fhsofhsdofihs dfhosi ifujp ise es esjf  fijdfoi   fheoihfosifpsdjfpsjfp"),
+        ),
+        // IntrinsicHeight(
+        //   child: Row(
+        //     crossAxisAlignment: CrossAxisAlignment.start,
+        //     children: [
+        //       Text(
+        //           "“  ",
+        //           style: GoogleFonts.spaceGrotesk(
+        //             fontWeight: FontWeight.w600,
+        //             fontSize: 100,
+        //             color: Colors.blue,
+        //
+        //           )
+        //       ),
+        //       SizedBox(
+        //         width: screenWidth(context, mulBy: 0.45),
+        //         child: RichText(
+        //           text: TextSpan(
+        //             text: "Flutter combines\n",
+        //             style: Theme.of(context).textTheme.bodyLarge,
+        //             children: [
+        //               TextSpan(
+        //                 text: "native performance and quality ",
+        //                   style: GoogleFonts.kumbhSans(
+        //                     fontWeight: FontWeight.w600,
+        //                     fontSize: 48,
+        //                     color: Colors.blue,
+        //                   )
+        //               ),
+        //               const TextSpan(
+        //                   text: "with "
+        //               ),
+        //               TextSpan(
+        //                   text: "high-velocity development ",
+        //                   style: GoogleFonts.kumbhSans(
+        //                     fontWeight: FontWeight.w600,
+        //                     fontSize: 48,
+        //                     color: Colors.blue,
+        //                   )
+        //               ),
+        //               const TextSpan(
+        //                   text: "and "
+        //               ),
+        //               TextSpan(
+        //                   text: "multi-platform reach",
+        //                   style: GoogleFonts.kumbhSans(
+        //                     fontWeight: FontWeight.w600,
+        //                     fontSize: 48,
+        //                     color: Colors.blue,
+        //                   )
+        //               ),
+        //               const TextSpan(
+        //                   text: "."
+        //               ),
+        //             ]
+        //           ),
+        //         ),
+        //       ),
+        //       Align(
+        //         alignment: Alignment.bottomRight,
+        //         child: Text(
+        //             "  ”",
+        //             softWrap: true,
+        //             textAlign: TextAlign.start,
+        //
+        //             style: GoogleFonts.spaceGrotesk(
+        //               fontWeight: FontWeight.w600,
+        //               fontSize: 100,
+        //               color: Colors.blue,
+        //
+        //             )
+        //         ),
+        //       ),
+        //     ],
+        //   ),
+        // )
       ],
     );
   }
