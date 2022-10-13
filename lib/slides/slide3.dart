2e1056fb727ad8b43d80997feb658a28fe60e95c
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-10-13 22:10:14 +0530

    Update slide3.dart

--- lib/slides/slide3.dart
+++ lib/slides/slide3.dart
@@ -21,61 +21,52 @@ class _Slide3State extends State<Slide3> {
       head: "All time challenges of mobile development",
       children: [
         SizedBox(
-          height: screenHeight(context, mulBy: 0.05),
+          height: screenHeight(context, mulBy: 0.1),
         ),
-        SizedBox(
-          width: screenWidth(context),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "      “To the metal” approaches",
-                      style: Theme.of(context).textTheme.displaySmall?.copyWith(
-                          color: Colors.blueGrey,
-                      ),
-                    ),
-                    SizedBox(
-                      height: screenHeight(context, mulBy: 0.04),
-                    ),
-                    const ListContent(head: "High-quality apps", sub: "Platform and system integrations", check: true),
-                    const ListContent(head: "High-performance UIs", sub: "Native code, GPU accelerated", check: true),
-                    const ListContent(head: "Must fund two apps", sub: "Two teams, codebases, & investments", check: false),
-                    const ListContent(head: "Inconsistent brand, features", sub: "Different across devices & OEMs", check: false),
-                  ],
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "      “To the metal” approaches",
+                  style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                      color: Colors.blueGrey,
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: screenWidth(context, mulBy: 0.1),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "      “Cross platform” approaches",
-                      style: Theme.of(context).textTheme.displaySmall?.copyWith(
-                        color: Colors.blueGrey,
-                      ),
-                    ),
-                    SizedBox(
-                      height: screenHeight(context, mulBy: 0.04),
-                    ),
-                    const ListContent(head: "Fast development", sub: "Same codebase, fast deployment", check: true),
-                    const ListContent(head: "Portability, reach", sub: "Single codebase", check: true),
-                    const ListContent(head: "Poor Performance", sub: "Slow, jerky, unpredictable", check: false),
-                    const ListContent(head: "Non-Native Look/Feel, features", sub: "Users can tell the difference", check: false),
-                  ],
+                SizedBox(
+                  height: screenHeight(context, mulBy: 0.04),
                 ),
-              ),
-            ],
-          ),
+                const ListContent(head: "High-quality apps", sub: "Platform and system integrations", check: true),
+                const ListContent(head: "High-performance UIs", sub: "Native code, GPU accelerated", check: true),
+                const ListContent(head: "Must fund two apps", sub: "Two teams, codebases, & investments", check: false),
+                const ListContent(head: "Inconsistent brand, features", sub: "Different across devices & OEMs", check: false),
+              ],
+            ),
+            //Spacer(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "      “Cross platform” approaches",
+                  style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                    color: Colors.blueGrey,
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight(context, mulBy: 0.04),
+                ),
+                const ListContent(head: "Fast development", sub: "Same codebase, fast deployment", check: true),
+                const ListContent(head: "Portability, reach", sub: "Single codebase", check: true),
+                const ListContent(head: "Poor Performance", sub: "Slow, jerky, unpredictable", check: false),
+                const ListContent(head: "Non-Native Look/Feel, features", sub: "Users can tell the difference", check: false),
+              ],
+            ),
+          ],
         )
       ],
     );
